ce5863d7ddb82d25b8f4113d5bb387da7b70e3e4
Jason Thompson <jason@jasont.us>
2020-06-20 19:41:31 -0400

    decrease posts per page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :comments
   has_many :likes
-  max_paginates_per 10
+  max_paginates_per 8
   #TODO posts can be pictures
 end
