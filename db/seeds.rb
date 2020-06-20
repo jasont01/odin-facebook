9bade0042674c7e68dfc47d3398c8ae17b820bb3
Jason Thompson <jason@jasont.us>
2020-06-20 16:37:22 -0400

    add demo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,61 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#TODO create seeds
+NUM_USERS = 50
 
-10.times do
-  u = User.create(:username => Faker::Internet.username, :email => Faker::Internet.email)
-  u.posts.create(:content => Faker::Lorem.paragraph(sentence_count: rand(1..5)))
+NUM_USERS.times do
+  #Users
+  u = User.create(:name => Faker::Name.name,
+                  :email => Faker::Internet.email,
+                  :image => Faker::Avatar.image,
+                  :password => Faker::Internet.password)
+  #Profiles
+  u.profile.update(:birthday => Faker::Date.birthday,
+                   :bio => Faker::Lorem.paragraph,
+                   :relationship_status => ["Single", "Married", "In a Relationship"].sample,
+                   :location => "#{Faker::Address.city}, #{Faker::Address.state_abbr}",
+                   :hometown => "#{Faker::Address.city}, #{Faker::Address.state_abbr}")
+
+  #Posts
+  rand(0..10).times { u.posts.create(:content => Faker::Lorem.paragraph(sentence_count: rand(1..5))) }
+end
+
+#Friends
+User.all.each do |user|
+  rand(0..(NUM_USERS / 2)).times do #set max number of friends to 50% of total number of users
+    loop do
+      friend = User.all.sample
+      if !user.friends.exists?(:friend_id => friend.id)
+        user.friends.create(:friend_id => friend.id)
+        friend.friends.create(:friend_id => user.id)
+        break
+      end
+    end
+  end
+end
+
+#Likes
+Post.all.each do |post|
+  rand(0..post.author.friends.count).times do
+    loop do
+      uid = User.all.sample.id
+      if !post.likes.exists?(:user_id => uid)
+        post.likes.create(:user_id => uid)
+        break
+      end
+    end
+  end
+end
+
+#Comments
+Post.all.each do |post|
+  rand(0..post.author.friends.count).times do
+    loop do
+      uid = User.all.sample.id
+      if !post.comments.exists?(:author_id => uid)
+        post.comments.create(:author_id => uid, :content => Faker::Lorem.sentence)
+        break
+      end
+    end
+  end
 end
